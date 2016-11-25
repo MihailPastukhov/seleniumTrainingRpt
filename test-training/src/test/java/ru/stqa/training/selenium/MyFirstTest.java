cd256f50b4b73d45549976a1f713e2cf74155d68
MihailPastukhov <michael6989@gmail.com>
2016-11-25 20:20:16 +0300

    Сделан сценарий, выполняющий действия в приложении litecart, описанные ниже.
    1) входит в панель администратора http://localhost/litecart/admin
    2) прокликивает последовательно все пункты меню слева, включая вложенные
    пункты
    3) для каждой страницы проверяет наличие заголовка

--- test-training/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ test-training/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,16 +1,17 @@
 package ru.stqa.training.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
     private WebDriver driver;
@@ -20,18 +21,53 @@ public class MyFirstTest {
     public void start(){
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @Test
     public void myFirstTest() {
+        goToAdminPage();
+        login();
+        menuItemsMovingTest();
+    }
 
-        driver.manage().window().maximize();
-        driver.get("http://localhost/litecart/admin/");
+    private void login() {
         driver.findElement(By.name("username")).clear();
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).clear();
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+    }
+
+    private int getMenuItemsCount(){
+        List<WebElement> menuItems = driver.findElements(By.xpath("//ul[@id='box-apps-menu']/li"));
+        int count=menuItems.size();
+        return count;
+    }
+    private int getSubMenuItemsCount(){
+        List<WebElement> menuItems = driver.findElements(By.xpath("//ul[@id='box-apps-menu']/li/ul/li"));
+        int subItems = menuItems.size();
+        return subItems;
+    }
+
+    private void menuItemsMovingTest(){
+
+        for (int i = 1; i < getMenuItemsCount()+1; i++){
+            driver.findElement(By.xpath("//ul[@id='box-apps-menu']/li[" + i + "]")).click();
+            Assert.assertTrue(driver.findElement(By.cssSelector("td#content h1")).isDisplayed());
+            if (getSubMenuItemsCount() > 0)
+            {
+                for (int j = 2; j < getSubMenuItemsCount() + 1; j++){
+                    driver.findElement(By.xpath("//ul[@id='box-apps-menu']/li/ul/li[" + j + "]")).click();
+                    Assert.assertTrue(driver.findElement(By.cssSelector("td#content h1")).isDisplayed());
+                }
+            }
+        }
+    }
+
+    private void goToAdminPage() {
+        driver.get("http://localhost/litecart/admin/");
     }
 
     @After
