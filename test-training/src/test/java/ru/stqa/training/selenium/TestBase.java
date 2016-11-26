4c5fd1ebaf7464d776b0c9ef29dc1aae64e63e0e
MihailPastukhov <michael6989@gmail.com>
2016-11-26 16:06:32 +0300

    Вспомогательные методы перенесены в базовый класс

--- test-training/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ test-training/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -81,4 +81,24 @@ public class TestBase {
         driver = null;
 
     }
+
+    protected void checkMainMenuStickerAvailability() {
+        boolean flag=true;
+        int count=0;
+        for (int i=1; i<getMainMenuPicturesCount() + 1; i++){
+            if (driver.findElements(By.xpath("(.//div[@class='image-wrapper']/div[contains(@class,'sticker')])["+i+"]")).size()!=1) {
+                count++;
+                flag=false;
+            }
+        }
+        System.out.println("Count of images without (or more than 1) stickers = " + count);
+        Assert.assertTrue(flag);
+        System.out.println(getMainMenuPicturesCount());
+    }
+
+    public int getMainMenuPicturesCount() {
+        List<WebElement> menuItems = driver.findElements(By.xpath("//div[@class='image-wrapper']"));
+        int mainMenuPicturesCount = menuItems.size();
+        return mainMenuPicturesCount;
+    }
 }
